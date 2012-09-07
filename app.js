c2c13d8d9b6d57e163fc5b64b3018e12a5d644af
jose armando gonzalez lopez <iam@armando.mx>
2012-09-07 18:30:25 -0500

    primer commit del chat con sencha touch y node js

---
+++ app.js
@@ -0,0 +1,66 @@
+var express = require('express'),
+	app =  express(),
+	http = require('http'),
+	server = http.createServer(app),
+	io = require('socket.io').listen(server),
+	fs = require('fs'),
+	connect = require('connect'),
+	port = process.env.PORT || 8080;
+
+app.configure(function() {
+    app.use(connect.static('./public'));
+	app.set('appIndex', './public/index.html');
+	app.use(connect.bodyParser());
+	app.use(express.logger());
+	app.use(express.errorHandler({ dumpExceptions: true }));
+});
+
+server.listen(port, function() {
+    console.log("Listening on " + port);
+});
+
+app.get('/', function(req, res) {
+	res.sendfile(app.set('appIndex'));
+});
+app.get('/index.html', function(req, res) {
+	res.sendfile(app.set('appIndex'))	
+});
+app.get('*', function(req, res) {
+    res.send('not found', 404);
+});
+
+// usernames which are currently connected to the chat
+var usernames = {};
+
+io.sockets.on('connection', function(socket) {
+
+	// when the client emits 'sendchat', this listens and executes
+	socket.on('sendchat', function(data) {
+		// we tell the client to execute 'updatechat' with 2 parameters
+		io.sockets.emit('updatechat', socket.username, data);
+	});
+
+	// when the client emits 'adduser', this listens and executes
+	socket.on('adduser', function(username) {
+		// we store the username in the socket session for this client
+		socket.username = username;
+		// add the client's username to the global list
+		usernames[username] = username;
+		// echo to client they've connected
+		socket.emit('updatechat', 'SERVER', 'Hi '+ username +' !');
+		// echo globally (all clients) that a person has connected
+		socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+		// update the list of users in chat, client-side
+		io.sockets.emit('updateusers', usernames);
+	});
+
+	// when the user disconnects.. perform this
+	socket.on('disconnect', function() {
+		// remove the username from global usernames list
+		delete usernames[socket.username];
+		// update list of users in chat, client-side
+		io.sockets.emit('updateusers', usernames);
+		// echo globally that this client has left
+		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+	});
+});
---
+++ public/app/view/Main.js
@@ -0,0 +1,62 @@
+Ext.define("Senchat.view.Main", {
+	extend: 'Ext.Panel',
+	requires: ['Ext.TitleBar'],
+	config: {
+
+		title: 'Welcome',
+		iconCls: 'home',
+
+		styleHtmlContent: true,
+		scrollable: true,
+
+		items: [{
+			docked: 'bottom',
+			xtype: 'toolbar',
+			items: [{
+				xtype: 'textfield',
+				placeHolder: '...',
+				flex: 5
+			}, {
+				xtype: 'button',
+				text: 'Send',
+				ui: 'forward',
+				iconMask: true,
+				margin: 5,
+				flex: 1,
+				handler: function(btn) {
+					var message = btn.up('container').items.items[0];
+					if(message.getValue()){
+						socket.emit('sendchat', message.getValue());
+						message.reset();
+					}
+				}
+			}]
+		}],
+		html: ['<div id="chat"></div>'].join("")
+	},
+	initialize: function() {
+		this.callParent(arguments);
+		this.configureSocket();
+	},
+	configureSocket : function() {
+		var me = this;
+		socket = io.connect('http://localhost:8080');
+		// on connection to server, ask for user's name with an anonymous callback
+		socket.on('connect', function() {
+			//ask the user the name when the client conects 
+			Ext.Msg.prompt('Welcome!', 'What\'s your name going to be today?', function(buttonId, value) {
+				if (buttonId == 'ok') {
+					socket.emit('adduser', value);
+				} else {
+					socket.emit('adduser', "Guess");
+				}
+			}, null, false, null, {
+				autoCapitalize: true,
+				placeHolder: 'Name please...'
+			});
+		});
+		socket.on('updatechat', function(username, data) {
+			Ext.get('chat').createChild('<b>' + username + ':</b> ' + data + '<br>');
+		});
+	}
+});
